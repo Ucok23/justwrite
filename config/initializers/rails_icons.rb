63fe101c5f3c0fe0251cb77123ec84edb31a3368
Ucok Isa Lubis <ulubis98@gmail.com>
2025-12-04 18:49:04 +0700

    icons default

--- config/initializers/rails_icons.rb
+++ config/initializers/rails_icons.rb
@@ -6,11 +6,11 @@ RailsIcons.configure do |config|
   # config.libraries.heroicons.default_variant = "" # Set a default variant for Heroicons
   # config.libraries.heroicons.exclude_variants = [:mini, :micro] # Exclude specific variants
 
-  # config.libraries.heroicons.outline.default.css = "size-6"
+  config.libraries.heroicons.outline.default.css = "size-4"
   # config.libraries.heroicons.outline.default.stroke_width = "1.5"
   # config.libraries.heroicons.outline.default.data = {}
 
-  # config.libraries.heroicons.solid.default.css = "size-6"
+  config.libraries.heroicons.solid.default.css = "size-4"
   # config.libraries.heroicons.solid.default.data = {}
 
   # config.libraries.heroicons.mini.default.css = "size-5"
